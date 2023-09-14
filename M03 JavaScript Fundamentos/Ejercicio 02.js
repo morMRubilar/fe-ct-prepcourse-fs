0bed1c5c4fabf4fa1d9301a4e553ed9111d72dd4
morMRubilar <mauricio.rubilar@gmail.com>
2023-09-14 17:46:44 -0300

    Ejercicio 1 y 2 terminados

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -1,9 +1,12 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
-function devolverString(string) {
+function devolverString(nombre) {
    // Debe retornar un string.
    // Tu código:
+   var devolverString = nombre;
+   var resultado = nombre;
+   return resultado
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +14,41 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+      var suma = x + y;
+      var resultado = suma;
+      return resultado 
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+      var resta = x - y;
+      var resultado = resta;
+      return resultado 
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+      var divide = x / y;
+      var resultado = divide;
+      return resultado 
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+      var multiplica = x * y;
+      var resultado = multiplica;
+      return resultado
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+      var obtenerResto = x % y;
+      var resultado = obtenerResto;
+      return resultado
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
